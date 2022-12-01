dc03ee69e2c28b78752db2690cf28a6ece6c04ff
Anastasiya Z <nastya_tim2201@mail.ru>
2022-12-01 19:27:03 +0300

    create method FillArrayShortStrings

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -11,8 +11,28 @@ array[i] = Console.ReadLine();
 }
 return array;
 }
- 
+string[] FillArrayShortStrings(string[] array)
+{
+   int count = 0; 
+   for (int i = 0; i < array.Length; i++)
+   {
+        if(array[i].Length <= 3) count++;
+   }
+   string[] newArray = new string[count];
+   count = 0;
+   for (int i = 0; i < array.Length; i++)
+   {
+        if(array[i].Length <= 3)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+   }
+   return newArray;
+}
+
 Console.WriteLine("УКажите желаемый размер массива - любое целое положительное число: ");
 int lenght = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите любые слова или наборы букв любой длины через Enter: ");
-string[] stringsMassive = CreateArrayFromUser(lenght);
+string[] strMassive = CreateArrayFromUser(lenght);
+string[] newStrMassive = FillArrayShortStrings(strMassive);
