35c4f80e6ce2b1f5b260f6d1e8c612d0cbeb53e9
Anastasiya Z <nastya_tim2201@mail.ru>
2022-12-01 19:17:09 +0300

    create method CreateArrayFromUser

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+// Первоначальный массив можно ввести с клавиатуры либо задать на старте выполнения алгоритма. 
+
+Console.Clear();
+string[] CreateArrayFromUser(int size)
+{
+string[] array = new string[size];
+for (int i = 0; i < size; i++)
+{
+array[i] = Console.ReadLine();
+}
+return array;
+}
+ 
+Console.WriteLine("УКажите желаемый размер массива - любое целое положительное число: ");
+int lenght = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите любые слова или наборы букв любой длины через Enter: ");
+string[] stringsMassive = CreateArrayFromUser(lenght);
